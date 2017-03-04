820f5c1639548c008a795dafbd440ac469a128a2
Lukas Herman <lukashh6@gmail.com>
2017-03-03 23:09:22 -0500

    Fix setup script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,15 @@
 echo "Making a directory at $HOME/bin"
 mkdir $HOME/bin
 
+# Download the source codes
+echo "Downloading the source code"
+wget https://raw.githubusercontent.com/lukashh6/makefile-generator/master/generator.py
+chmod +x generator.py
+
+# Linking to the bin dir
+echo "Linking to $HOME/bin/generate"
+ln -s generator.py $HOME/bin/generate
+
 echo "Exporting PATH"
 echo "export PATH=\$PATH:$HOME/bin" >> $HOME/.bash_profile
 
