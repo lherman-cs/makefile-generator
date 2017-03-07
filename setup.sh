96177f04b6ca957089b8b525009e48735b7705a1
Lukas Herman <lukashh6@gmail.com>
2017-03-07 08:57:09 -0500

    Fix the broken link

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ cd $INST_PATH
 # Download the source codes
 echo "Downloading the source code"
 wget https://raw.githubusercontent.com/lukashh6/makefile-generator/master/generator.py
-chmod +x generator.py
+chmod +x $INST_PATH/generator.py
 
 # Make a bin dir in home directory
 echo "Making a directory at $HOME/bin"
@@ -18,10 +18,13 @@ mkdir $HOME/bin
 
 # Linking to the bin dir
 echo "Linking to $HOME/bin/generate"
-ln -s generator.py $HOME/bin/generate
+ln -s $INST_PATH/generator.py $HOME/bin/generate
 
 echo "Exporting PATH"
 echo "export PATH=\$PATH:$HOME/bin" >> $HOME/.bash_profile
+
+# Refresh bash profile
+source $HOME/.bash_profile
 
 echo "Done!"
 echo
